8e12765aaaf19c43f67ca42a8bee56b8f078e366
gustavovelascoh <gustavo.velascoh@gmail.com>
2018-11-29 11:49:05 +0000

    files renamed

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+ * File:   main.c
+ * Author: Gustavo Velasco-Hernandez
+ *
+ * Created on 11/29/2018 11:17:32 AM UTC
+ */
+
+#include <xc.h>
+#include "main_cfg.h"
+#include "suart.h"
+
+
+void main(void) {
+
+    while (1){
+
+        __delay_ms(300);
+
+        suart_tx_str("Hola\n",5);
+    }
+    return;
+}
